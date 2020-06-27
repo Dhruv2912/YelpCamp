8fea133bcfd988f3489451b2f3474e089d17f4f6
dhruv <dhruvpopat2912@gmail.com>
2020-06-26 20:49:13 -0700

    Added GET Route for adding new comments using form

--- v3/app.js
+++ v3/app.js
@@ -26,7 +26,7 @@ app.get("/campgrounds", function (req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render("index", { campgrounds: allCampgrounds });
+      res.render("campgrounds/index", { campgrounds: allCampgrounds });
     }
   });
 });
@@ -50,7 +50,7 @@ app.post("/campgrounds", function (req, res) {
 
 //NEW- show form to create new campground
 app.get("/campgrounds/new", function (req, res) {
-  res.render("new.ejs");
+  res.render("campgrounds/new");
 });
 
 //SHOW- shows more info about one campground
@@ -63,9 +63,24 @@ app.get("/campgrounds/:id", function (req, res) {
         console.log(err);
       } else {
         console.log(foundCampground);
-        res.render("show", { campground: foundCampground });
+        res.render("campgrounds/show", { campground: foundCampground });
       }
     });
+});
+
+//==========================================
+// Comment Routes
+//==========================================
+
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+  //find campground by id
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", { campground: campground });
+    }
+  });
 });
 
 //Port
